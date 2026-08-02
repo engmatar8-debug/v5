8ec9bc007c00d60692e8ee0de267449c40ede141
engmatar8-debug <engmatar8@gmail.com>
2026-08-02 23:37:54 +0300

    ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -250,7 +250,10 @@ export default function ContactPage() {
                         </div>
                         <div>
                           <p className="text-brand-textMuted text-xs mb-0.5">Headquarters</p>
-                          <p className="text-white text-sm font-medium">Kuala Lumpur, Malaysia</p>
+                          <p className="text-white text-sm font-medium">Gedung Jaya Lantai 5 unit A.6 JL. M.H Thamrin 
+No.12 , RT.002/RW.001, Kb. Sirih, Kec. Menteng, 
+Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 
+10340</p>
                         </div>
                       </div>
 
